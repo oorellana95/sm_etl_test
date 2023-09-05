87250048d666347080771c5434cbc90186b511dc
oorellana95 <oorellana30.95@gmail.com>
2023-09-05 14:41:47 -0500

    update database and get data engineering schema

--- database/init.sql
+++ database/init.sql
@@ -7,30 +7,59 @@ CREATE DATABASE culinary_recipes_mysql;
 USE culinary_recipes_mysql;
 
 --
--- Table structure for table `recipes`
+-- Table structure for table `job_title`
+--
+
+CREATE TABLE `job_title` (
+  `id` bigint NOT NULL,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL,
+  `id_encoded` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `sex` enum('Male','Female') NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `id_job_title` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `user_fk_1` FOREIGN KEY (`id_job_title`) REFERENCES `job_title` (`id`)
+);
+
+--
+-- Table structure for table `recipe`
 --
 
 CREATE TABLE `recipe` (
   `id` bigint NOT NULL,
-  `contributor_id` bigint NOT NULL,
+  `id_user` bigint NOT NULL,
   `name` varchar(100) NOT NULL,
   `minutes` int NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `steps` json DEFAULT NULL,
   `nutrition` json DEFAULT NULL,
   `calorie_level` int NOT NULL,
-  `created_at` datetime NOT NULL,
+  `submitted_at` datetime NOT NULL,
   `last_updated` datetime NOT NULL,
   PRIMARY KEY (`id`),
+  CONSTRAINT `recipe_fk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
   CONSTRAINT `recipe_chk_1` CHECK (0<=`calorie_level` AND `calorie_level`<=2)
 );
 
 --
--- Table structure for table `ratings`
+-- Table structure for table `rating`
 --
 
 CREATE TABLE `rating` (
-  `id` bigint NOT NULL,  
+  `id` bigint NOT NULL,
   `id_user` bigint NOT NULL,
   `id_recipe` bigint NOT NULL,
   `valuation` int NOT NULL,
@@ -38,12 +67,13 @@ CREATE TABLE `rating` (
   `submitted_date` date NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`id_user`,`id_recipe`),
-  CONSTRAINT `ratings_fk_1` FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id`),
-  CONSTRAINT `ratings_chk_1` CHECK (0<=`valuation` AND `valuation`<=5)
+  CONSTRAINT `rating_fk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  CONSTRAINT `rating_fk_2` FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id`),
+  CONSTRAINT `rating_chk_1` CHECK (0<=`valuation` AND `valuation`<=5)
 );
 
 --
--- Table structure for table `ingredients`
+-- Table structure for table `ingredient`
 --
 
 CREATE TABLE `ingredient` (
@@ -65,7 +95,7 @@ CREATE TABLE `recipe_ingredient` (
 );
 
 --
--- Table structure for table `tags`
+-- Table structure for table `tag`
 --
 
 CREATE TABLE `tag` (
