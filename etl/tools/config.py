0d34fff138e07303c83508368050d61724102250
oorellana95 <oorellana30.95@gmail.com>
2023-09-05 17:27:47 -0500

    Add temp. config

---
+++ etl/tools/config.py
@@ -0,0 +1,7 @@
+import os
+
+PATH_ROOT_DIR = os.path.abspath("")
+PATH_INPUT_DIR = os.path.join(PATH_ROOT_DIR, "data_test")
+RAW_USERS_PATH = os.path.join(PATH_INPUT_DIR, "RAW_users.csv")
+RAW_RECIPES_PATH = os.path.join(PATH_INPUT_DIR, "RAW_recipes.csv")
+RAW_INTERACTIONS_PATH = os.path.join(PATH_INPUT_DIR, "RAW_interactions.csv")
