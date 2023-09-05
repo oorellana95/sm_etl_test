f1716c9c528c67d7510b160853d872a8dc31dc14
oorellana95 <oorellana30.95@gmail.com>
2023-09-05 18:56:21 -0500

    Add column_checker.py class

---
+++ etl/process_file/column_checker.py
@@ -0,0 +1,12 @@
+"""
+Attributes Class
+Creates a standardized class to check columns from a dataframe.
+"""
+from typing import Any, Optional
+
+
+class ColumnChecker:
+    def __init__(self, name: str, value_type: str, check_function: Optional[Any]):
+        self.name = name
+        self.value_type = value_type # Note, it follows the pandas dtype convention. object = str or mixed
+        self.check_function = check_function
