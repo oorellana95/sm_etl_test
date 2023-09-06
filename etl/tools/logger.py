cc34d0689ba95a36c96d24f16891eea922a0bacd
oorellana95 <oorellana30.95@gmail.com>
2023-09-05 19:41:44 -0500

    Add additional_information field to logger.py

--- etl/tools/logger.py
+++ etl/tools/logger.py
@@ -11,37 +11,63 @@ class Logger:
         return cls
 
     @classmethod
-    def error(cls, message: str, code: str = None):
+    def error(cls, message: str, code: str = None, additional_information: dict = None):
         """Log an error message with the standard output format."""
         cls._logger.setLevel(logging.ERROR)
-        output = cls.output(logger_type="Error", message=message, code=code)
+        output = cls.output(
+            logger_type="Error",
+            message=message,
+            code=code,
+            additional_information=additional_information,
+        )
         cls._logger.error(output)
 
     @classmethod
-    def warning(cls, message: str, code: str = None):
+    def warning(
+        cls, message: str, code: str = None, additional_information: dict = None
+    ):
         """Log a warning message with the standard output format."""
         cls._logger.setLevel(logging.WARNING)
-        output = cls.output(logger_type="Warning", message=message, code=code)
+        output = cls.output(
+            logger_type="Warning",
+            message=message,
+            code=code,
+            additional_information=additional_information,
+        )
         cls._logger.warning(output)
 
     @classmethod
-    def debug(cls, message: str, code: str = None):
+    def debug(cls, message: str, code: str = None, additional_information: dict = None):
         """Log a debug message with the standard output format."""
         cls._logger.setLevel(logging.DEBUG)
-        output = cls.output(logger_type="Debug", message=message, code=code)
+        output = cls.output(
+            logger_type="Debug",
+            message=message,
+            code=code,
+            additional_information=additional_information,
+        )
         cls._logger.debug(output)
 
     @classmethod
-    def info(cls, message: str, code: str = None):
+    def info(cls, message: str, code: str = None, additional_information: dict = None):
         """Log an information message with the standard output format."""
         cls._logger.setLevel(logging.INFO)
-        output = cls.output(logger_type="Info", message=message, code=code)
+        output = cls.output(
+            logger_type="Info",
+            message=message,
+            code=code,
+            additional_information=additional_information,
+        )
         cls._logger.info(output)
 
     @classmethod
-    def output(cls, logger_type: str, message: str, code: str):
+    def output(
+        cls, logger_type: str, message: str, code: str, additional_information: dict
+    ):
         """Output message with code and value."""
         output = f"{logger_type} in {cls._project}:{cls._service};"
         if code:
             output = output + f"\n{logger_type} code: {code};"
+        if additional_information:
+            output = output + f"\n{additional_information};"
         return output + f"\nMessage: {message}"
