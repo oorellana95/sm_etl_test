71376a9c206289f755ae0455d45cea9688a4c794
oorellana95 <oorellana30.95@gmail.com>
2023-09-05 19:41:55 -0500

    Add comments to validation_functions.py

--- etl/tools/validation_functions.py
+++ etl/tools/validation_functions.py
@@ -1,11 +1,12 @@
 """
 Validation functions:
-The functions below are all meant to check specific type of data. They can expect a list or just a single value.
+The functions are all meant to check specific type of data. They can expect a list or just a single value.
 """
 import datetime
 
 
 def check_is_date(date_text: str):
+    """Check if the value is a date. Format YYYY-MM-DD."""
     try:
         datetime.datetime.strptime(date_text, "%Y-%m-%d")
     except ValueError:
@@ -13,10 +14,12 @@ def check_is_date(date_text: str):
 
 
 def check_contains_all_date(date_column: list):
+    """Check if the list of values are dates. Format YYYY-MM-DD."""
     (check_is_date(value) for value in date_column)
 
 
 def check_contains_all_valid_ranking_numbers(integer_column: list):
+    """Check if the list of values are in between the ranking range 0-5."""
     min_ranking_value, max_ranking_value = 0, 4
     if (
         min(integer_column) < min_ranking_value
