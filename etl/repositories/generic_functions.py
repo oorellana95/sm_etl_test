5bb5e3430c445705f1c6868ced39bc44853bb42f
oorellana95 <oorellana30.95@gmail.com>
2023-09-07 08:45:39 -0500

    Add upsert function

---
+++ etl/repositories/generic_functions.py
@@ -0,0 +1,39 @@
+def upsert_data(db_session, model, new_entries, primary_key_name="id"):
+    # Extract primary key values from new entries
+    new_primary_key_values = [entry.get(primary_key_name) for entry in new_entries]
+
+    # Query for existing entries using the primary key values
+    existing_entries = (
+        db_session.query(model)
+        .filter(getattr(model, primary_key_name).in_(new_primary_key_values))
+        .all()
+    )
+
+    # Create a dictionary to efficiently access existing entries by primary key
+    existing_entries_dict = {
+        getattr(existing_entry, primary_key_name): existing_entry
+        for existing_entry in existing_entries
+    }
+
+    # Prepare a list for new entries to be inserted
+    entries_to_insert = []
+
+    # Iterate through new entries and perform upsert
+    for new_entry in new_entries:
+        primary_key_value = new_entry.get(primary_key_name)
+        existing_entry = existing_entries_dict.get(primary_key_value)
+
+        if existing_entry:
+            # Update existing entry with new data
+            for key, value in new_entry.items():
+                setattr(existing_entry, key, value)
+        else:
+            # Add new entry to the list for bulk insertion
+            entries_to_insert.append(new_entry)
+
+    # Bulk insert new entries if there are any
+    if entries_to_insert:
+        db_session.bulk_insert_mappings(model, entries_to_insert)
+
+    # Commit changes to the database session
+    db_session.commit()
