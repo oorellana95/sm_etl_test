31e45fdd8b145ff745c7cf541aa230a8e2eddd46
oorellana95 <oorellana30.95@gmail.com>
2023-09-07 14:29:09 -0500

    Upsert rating data

--- etl/repositories/rating.py
+++ etl/repositories/rating.py
@@ -4,7 +4,10 @@ Rating repository module
 import pandas as pd
 
 from etl.models.rating import Rating
-from etl.repositories.generic_functions import upsert_data, protect_session_with_rollback
+from etl.repositories.generic_functions import (
+    protect_session_with_rollback,
+    upsert_data,
+)
 
 
 @protect_session_with_rollback
@@ -17,13 +20,17 @@ def load_ratings(db_session, ratings_df: pd.DataFrame):
             "recipe_id": "id_recipe",
             "date": "submitted_at",
             "rating": "valuation",
-            "review": "review"
+            "review": "review",
         }
     )
 
     # Convert the DataFrame to a list of dictionaries
     entries = ratings_df.to_dict(orient="records")
 
-    # Upsert the data into the Recipe table
-    upsert_data(db_session, Rating, entries)
-
+    # Upsert the data into the Rating table
+    upsert_data(
+        db_session=db_session,
+        model=Rating,
+        new_entries=entries,
+        primary_key_names=("id_user", "id_recipe"),
+    )
