0b04cdcb8de894e46b4b4b39aa9514b9d90c4b3b
oorellana95 <oorellana30.95@gmail.com>
2023-09-10 11:36:21 -0500

    Add default returns for validation functions and refactor is_list_of_strings and is_list_of_floats to is_list_of_type

--- etl/services/general_functions/validation.py
+++ etl/services/general_functions/validation.py
@@ -10,6 +10,7 @@ def is_date(single_date_str: str):
     """Check if the value is a date. Format YYYY-MM-DD."""
     try:
         datetime.datetime.strptime(single_date_str, "%Y-%m-%d")
+        return True
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
 
@@ -18,6 +19,7 @@ def contains_all_dates(list_dates_str: list):
     """Check if the list of values are dates. Format YYYY-MM-DD."""
     for value in list_dates_str:
         is_date(value)
+    return True
 
 
 def contains_all_valid_ranking_numbers(list_integers: list):
@@ -27,57 +29,45 @@ def contains_all_valid_ranking_numbers(list_integers: list):
         raise ValueError(
             f"Incorrect value, should be between {min_ranking_value} and {max_ranking_value}"
         )
+    return True
 
 
-def is_list_of_strings(single_str_arr_str: list):
-    """Check if the value is a list of strings."""
+def is_list_of_type(single_str_arr_type: str, type_to_check):
+    """Check if the value is a list of the given type."""
     try:
-        parsed_list = eval(single_str_arr_str)
+        parsed_list = eval(single_str_arr_type)
         if not isinstance(parsed_list, list) or not all(
-            isinstance(item, str) for item in parsed_list
+            isinstance(item, type_to_check) for item in parsed_list
         ):
             raise ValueError(
-                f"The value '{single_str_arr_str}' is not a list of strings."
+                f"The value '{single_str_arr_type}' is not a list of {type_to_check}."
             )
-    except (ValueError, SyntaxError):
+        return True
+    except (ValueError, SyntaxError, NameError):
         raise ValueError(
-            f"Incorrect value, should be a list of strings: '{single_str_arr_str}'"
+            f"Incorrect value, should be a list of {type_to_check}: '{single_str_arr_type}'"
         )
 
 
-def contains_list_of_strings(list_strings: list):
-    """Check if the list of values are lists of strings."""
-    for value in list_strings:
-        is_list_of_strings(value)
+def contains_list_of_strings(list_str_arr_str: list):
+    """Check if the list of values are lists of strings. Input example: ['['apple', 'banana']', '['cherry', 'date']']"""
+    for value in list_str_arr_str:
+        is_list_of_type(single_str_arr_type=value, type_to_check=str)
+    return True
 
 
-def is_list_of_floats(single_str_arr_float: list):
-    """Check if the value is a list of floats."""
-    try:
-        parsed_list = eval(single_str_arr_float)
-        if not isinstance(parsed_list, list) or not all(
-            isinstance(item, float) for item in parsed_list
-        ):
-            raise ValueError(
-                f"The value '{single_str_arr_float}' is not a list of floats."
-            )
-    except (ValueError, SyntaxError):
-        raise ValueError(
-            f"Incorrect value, should be a list of floats: '{single_str_arr_float}'"
-        )
-
-
-def contains_list_of_floats(list_strings: list):
-    """Check if the list of values are lists of floats."""
-    for value in list_strings:
-        is_list_of_floats(value)
+def contains_list_of_floats(list_str_arr_float: list):
+    """Check if the list of values are lists of floats. Input example: ['['3.3', '2.1']', '['0.3', '7.9']']"""
+    for value in list_str_arr_float:
+        is_list_of_type(single_str_arr_type=value, type_to_check=float)
+    return True
 
 
 def is_valid_email(email):
     """Check if the value is a valid email address."""
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     if re.match(pattern, email):
-        return
+        return True
     raise ValueError("Incorrect data format, should be a valid email")
 
 
@@ -85,12 +75,13 @@ def contains_list_of_emails(list_emails: list):
     """Check if the list of values are valid email addresses."""
     for email in list_emails:
         is_valid_email(email)
+    return True
 
 
 def is_valid_sex(sex):
     """Check if the value is a valid sex (male or female)."""
     if sex.lower() in ("male", "female"):
-        return
+        return True
     raise ValueError("Incorrect data format, should be a valid sex (male or female)")
 
 
@@ -98,3 +89,4 @@ def contains_list_of_sex_values(sex_values: list):
     """Check if the list of values are valid sex values (male or female)."""
     for sex in sex_values:
         is_valid_sex(sex)
+    return True
